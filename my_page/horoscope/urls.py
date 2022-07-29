9b19c0f4f8534e6b2f06c31c5538ff6ea5f60e7b
nyckolas <nyckolas.work@gmail.com>
2022-07-29 21:23:14 +0300

    Add converter routing for horoscope

--- my_page/horoscope/urls.py
+++ my_page/horoscope/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 from . import views as views_horoscope
 
+
 urlpatterns = [
-    path('<sign_zodiac>/', views_horoscope.get_zodiac),
-    # path('leo/', views_horoscope.leo),
-    # path('scorpio/', views_horoscope.scorpio),
-    # path('aquarius/', views_horoscope.aquarius),
-    # path('taurus/', views_horoscope.taurus),
-    # path('aries/', views_horoscope.aries),
-    
+    path('<int: sign_zodiac>/', views_horoscope.get_zodiac_by_number),
+    path('<str: sign_zodiac>/', views_horoscope.get_zodiac),
 ]
--- my_page/horoscope/views.py
+++ my_page/horoscope/views.py
@@ -1,27 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.http import HttpResponse, HttpResponseNotFound
 
-def get_zodiac(request, sign_zodiac):
-    if sign_zodiac == 'leo':
-        return HttpResponse("""Zodiac sign - Leo.</br>
-                        Leo is the fifth sign of the zodiac, the Sun (July 23 to August 21).
-                        """)
-    elif sign_zodiac == 'scorpio':
-        return HttpResponse("""Zodiac sign - Scorpio.</br>
-                        Scorpio is the eighth sign of the zodiac, the planet Mars (October 24 to November 22).
-                        """)
-    elif sign_zodiac == 'aquarius':
-        return HttpResponse("""Zodiac sign - Aquarius.</br>
-                        Aquarius is the eleventh sign of the zodiac, the planets Uranus and Saturn (January 21 to February 19).
-                        """)
-    elif sign_zodiac == 'aries':
-        return HttpResponse("""Zodiac sign - Aries.</br>
-                        Aries is the first sign of the zodiac, the planet Mars (March 21 to April 20).
-                        """)
-    elif sign_zodiac == 'taurus':
-        return HttpResponse("""Zodiac sign - Taurus.</br>
-                        Taurus is the second sign of the zodiac, the planet Venus (from April 21 to May 21).
-                        """)
+
+zodiac_dict = {
+    'aries': 'Zodiac sign - Aries is the first sign of the zodiac, the planet Mars (March 21 to April 20).',
+    'taurus': 'Zodiac sign - Taurus is the second sign of the zodiac, the planet Venus (from April 21 to May 21).',
+    'gemini': 'Zodiac sign - Gemini is the third sign of the zodiac, the planet Mercury (May 22 to June 21).',
+    'cancer': 'Zodiac sign - Cancer is the fourth sign of the zodiac, the Moon (June 22 to July 22).',
+    'leo': 'Zodiac sign - Leo is the fifth sign of the zodiac, the Sun (July 23 to August 21).',
+    'virgio': 'Zodiac sign - Virgo is the sixth sign of the zodiac, the planet Mercury (August 22 to September 23).',
+    'libra': 'Zodiac sign - Libra is the seventh sign of the zodiac, the planet Venus (September 24 to October 23).',
+    'scorpio': 'Zodiac sign - Scorpio is the eighth sign of the zodiac, the planet Mars (October 24 to November 22).',
+    'sagittarius': 'Zodiac sign - Sagittarius is the ninth sign of the zodiac, the planet Jupiter (November 23 to December 22).',
+    'capricorn': 'Zodiac sign - Capricorn is the tenth sign of the zodiac, the planet Saturn (December 23 to January 20).',
+    'aquarius': 'Zodiac sign - Aquarius is the eleventh sign of the zodiac, the planets Uranus and Saturn (January 21 to February 19).',
+    'pisces': 'Zodiac sign - Pisces is the twelfth sign of the zodiac, the planet Jupiter (February 20 to March 20).',
+}
+
+
+def get_zodiac_by_number(request, sign_zodiac: int):
+    description = zodiac_dict.get(sign_zodiac, None)
+    if description:
+        return HttpResponse(description)
     else:
-        return HttpResponse(f"It is not zodiac sign - {sign_zodiac}")
+        return HttpResponseNotFound(f"It is not zodiac sign - {sign_zodiac}")
+   
+ 
+def get_zodiac(request, sign_zodiac: str):
+    description = zodiac_dict.get(sign_zodiac, None)
+    if description:
+        return HttpResponse(description)
+    else:
+        return HttpResponseNotFound(f"It is not zodiac sign - {sign_zodiac}")
