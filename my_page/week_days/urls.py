09a84b8d45c1029c1d6585586ff787c5830aae31
nyckolas <nyckolas.work@gmail.com>
2022-07-29 21:22:31 +0300

    Add dynamic routing for week_days

--- my_page/week_days/urls.py
+++ my_page/week_days/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views as views_week_days
 
 urlpatterns = [
-    path('monday/', views_week_days.monday),
-    path('tuesday/', views_week_days.tuesday),
+    path('<day_name>/', views_week_days.get_day_name),
     
 ]
--- my_page/week_days/views.py
+++ my_page/week_days/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def monday(request):
-    return HttpResponse("Monday")
-
-def tuesday(request):
-    return HttpResponse("Tuesday")
+def get_day_name(request, day_name):
+    day_list = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+    if day_name.lower() in day_list:
+        return HttpResponse(f"{day_name.capitalize()}")
+    else:
+        return HttpResponse(f"I don't know such a day of the week.")
