9133c6407458cbb291d37d6f7bdbe4bc57b01e95
nyckolas <nyckolas.work@gmail.com>
2022-07-30 10:47:35 +0300

    Add redirect geometry

--- my_page/geometry/urls.py
+++ my_page/geometry/urls.py
@@ -3,8 +3,10 @@ from . import views as calulate_views
 
 
 urlpatterns = [
-    path('rectangle/<int:width>/<int:height>/', calulate_views.get_rectangle_area),
-    path('square/<int:width>/', calulate_views.get_square_area),
-    path('circle/<int:radius>/', calulate_views.get_circle_area),
-
+    path('rectangle/<int:width>/<int:height>', calulate_views.rectangle),
+    path('square/<int:width>', calulate_views.square),
+    path('circle/<int:radius>', calulate_views.circle),
+    path('get_rectangle_area/<int:width>/<int:height>', calulate_views.get_rectangle_area),
+    path('get_square_area/<int:width>', calulate_views.get_square_area),
+    path('get_circle_area/<int:radius>', calulate_views.get_circle_area),
 ]
--- my_page/geometry/views.py
+++ my_page/geometry/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from math import pi
 
 def get_rectangle_area(request, width: int, height: int):
-    return HttpResponse(f"The area of the rectangle {width}x{height} is equal - {width*height}")
+    return HttpResponseRedirect(f"/calculate_geometry/rectangle/{width}/{height}")
 
 def get_square_area(request, width: int):
-    return HttpResponse(f"The area of the square {width}x{width} is equal - {width**2}")
+    return HttpResponseRedirect(f"/calculate_geometry/square/{width}/{width}")
 
 def get_circle_area(request, radius: int):
+    return HttpResponseRedirect(f"/calculate_geometry/circle/{radius}")
+
+def rectangle(request, width: int, height: int):
+    return HttpResponse(f"The area of the rectangle {width}x{height} is equal - {width*height}")
+
+def square(request, width: int):
+    return HttpResponse(f"The area of the square {width}x{width} is equal - {width**2}")
+
+def circle(request, radius: int):
     return HttpResponse(f"The area of the circle with radius {radius} is equal - {pi*radius**2}")
