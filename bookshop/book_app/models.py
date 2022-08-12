bcf7dab411039d099652b217e6b5a0ef4bfc0e53
nyckolas <nyckolas.work@gmail.com>
2022-08-12 11:47:24 +0300

    Add field author to models.Book

--- bookshop/book_app/models.py
+++ bookshop/book_app/models.py
@@ -6,6 +6,7 @@ class Book(models.Model):
     title = models.CharField(max_length=70)
     rating = models.IntegerField()
     is_best_selling = models.BooleanField()
+    author = models.CharField(max_length=100, null=True)
 
 
     def __str__(self) -> str:
