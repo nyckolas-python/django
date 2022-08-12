4d4bc72e1d5af06b70b8089bde96a18741d97848
nyckolas <nyckolas.work@gmail.com>
2022-08-12 11:17:12 +0300

    Add __str__ to models.Book

--- bookshop/book_app/models.py
+++ bookshop/book_app/models.py
@@ -5,4 +5,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=70)
     rating = models.IntegerField()
-    
+
+
+    def __str__(self) -> str:
+        return f"{self.title} - {self.rating}%"
