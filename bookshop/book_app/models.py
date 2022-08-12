f5266a302c45184407957421481ae2e930c3efbb
nyckolas <nyckolas.work@gmail.com>
2022-08-12 11:38:29 +0300

    Add field is_best_seller to models.Book

--- bookshop/book_app/models.py
+++ bookshop/book_app/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=70)
     rating = models.IntegerField()
+    is_best_selling = models.BooleanField()
 
 
     def __str__(self) -> str:
-        return f"{self.title} - {self.rating}%"
+        return f"{self.title} - {self.rating}% - bestSeller - {self.is_best_selling}"
