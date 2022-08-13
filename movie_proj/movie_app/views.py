21ef8dec3a4a09891319288c4e128453513af7e0
nyckolas <nyckolas.work@gmail.com>
2022-08-13 12:14:09 +0300

    Add show_one_movie view

--- movie_proj/movie_app/views.py
+++ movie_proj/movie_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Movie
@@ -8,4 +8,11 @@ def show_all_movies(request):
     movies = Movie.objects.all()
     return render(request, 'movie_app/all_movies.html', {
 		'movies': movies
+	})
+
+
+def show_one_movie(request, id_movie:int):
+    movie = get_object_or_404(Movie, id=id_movie)
+    return render(request, 'movie_app/one_movie.html', {
+		'movie': movie
 	})
