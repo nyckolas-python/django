e281b1514fe40a5c02266637abc2790c5ab8bfef
nyckolas <nyckolas.work@gmail.com>
2022-08-14 22:01:26 +0300

    Add admin Movie

--- movie_proj/movie_app/admin.py
+++ movie_proj/movie_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Movie
 # Register your models here.
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'rating', 'year', 'budget']
+    list_editable = ['rating', 'year', 'budget']
+    ordering =['-rating', 'name']
+    list_per_page = 3
+
+# admin.site.register(Movie, MovieAdmin)
