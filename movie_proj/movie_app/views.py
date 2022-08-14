c99ff6fb8fe5577c0adb70aaa9a50d1530c32818
nyckolas <nyckolas.work@gmail.com>
2022-08-14 16:45:03 +0300

    Add annotate func

--- movie_proj/movie_app/views.py
+++ movie_proj/movie_app/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import render, get_object_or_404
-from django.db.models import F, Sum, Max, Min, Avg, Count
+from django.db.models import F, Sum, Max, Min, Avg, Count, Value
 
 # Create your views here.
 from .models import Movie
 
 
 def show_all_movies(request):
-    movies = Movie.objects.order_by(F('year').desc(nulls_last=True), '-rating')
-    agg = movies.aggregate(Avg('budget'), Max('rating'), Min('rating'), Count('id'))
+    # movies = Movie.objects.order_by(F('year').desc(nulls_last=True), '-rating')
+    movies = Movie.objects.annotate(
+        true_bool=Value(True),
+        false_bool=Value(False),
+        str_field=Value('Hello Django'),
+        int_field=Value(1234567890),
+        new_budget=F('budget')+100999,     
+        
+    ).annotate(rating_plus_year=F('rating')+F('year'),)
+
+    agg = movies.aggregate(Avg('budget'), Max(
+        'rating'), Min('rating'), Count('id'))
     return render(request, 'movie_app/all_movies.html', {
-		'movies': movies,
-		'agg': agg,
-	})
+        'movies': movies,
+      		'agg': agg,
+    })
 
 
-def show_one_movie(request, slug_movie:str):
+def show_one_movie(request, slug_movie: str):
     movie = get_object_or_404(Movie, slug=slug_movie)
     return render(request, 'movie_app/one_movie.html', {
-		'movie': movie
-	})
+        'movie': movie
+    })
