78b1060c4b77c87705f4de30af392af8f37c5dbc
nyckolas <nyckolas.work@gmail.com>
2022-08-14 22:02:22 +0300

    Update language

--- movie_proj/movie_proj/urls.py
+++ movie_proj/movie_proj/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'Моя админка'
+admin.site.index_title = 'Моя супер админка'
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
