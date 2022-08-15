c1f4ffef55d28df7b5ad8a68959483ee565b5b20
nyckolas <nyckolas.work@gmail.com>
2022-08-15 21:38:06 +0300

    Add new route for directors

--- movie_proj/movie_app/urls.py
+++ movie_proj/movie_app/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.show_all_movies),
     path('movie/<str:slug_movie>', views.show_one_movie, name='movie_detail'),
+    path('directors/', views.show_all_directors),
+    path('directors/<int:id_director>', views.show_one_director, name='director_detail'),   
 
 ]
