03b02877a92e045b1795e3c0e4b0686cee8c4b77
nyckolas <nyckolas.work@gmail.com>
2022-08-16 15:49:35 +0300

    Add new route for actors

--- movie_proj/movie_app/urls.py
+++ movie_proj/movie_app/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('', views.show_all_movies),
     path('movie/<str:slug_movie>', views.show_one_movie, name='movie_detail'),
     path('directors/', views.show_all_directors),
-    path('directors/<int:id_director>', views.show_one_director, name='director_detail'),   
+    path('directors/<int:id_director>',
+         views.show_one_director, name='director_detail'),
+    path('actors/', views.show_all_actors),
+    path('actors/<int:id_actor>', views.show_one_actor, name='actor_detail'),
 
 ]
